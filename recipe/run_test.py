93be20800a81f8c9c08e1f23829c42a4c00dec3d
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-09-21 16:02:07 -0500

    clean up test script

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,6 +1,9 @@
 import os
-import pytest
+import sys
+import subprocess
 from pathlib import Path
+
+import pytest
 
 COV_FAIL_UNDER = 70
 
@@ -8,24 +11,24 @@ HERE = Path(__file__).parent
 SRC = HERE / "src"
 CACHE_DIR = Path(os.environ["SRC_DIR"]) / ".yarn-cache"
 PYTEST_ARGS = [
-  "-vv", 
-  "--asyncio-mode=auto", 
-  "--cov=jupyter_ydoc", 
-  "--cov-report=term-missing:skip-covered", 
-  "--no-cov-on-fail", 
-  f"--cov-fail-under={COV_FAIL_UNDER}"
+    "-vv",
+    "--asyncio-mode=auto",
+    "--cov=jupyter_ydoc",
+    "--cov-report=term-missing:skip-covered",
+    "--no-cov-on-fail",
+    f"--cov-fail-under={COV_FAIL_UNDER}",
 ]
 
 os.environ["YARN_CACHE_FOLDER"] = str(CACHE_DIR)
 
 for cmd in [
-  ["yarn"],
-  ["yarn", "lerna", "bootstrap"],
-  ["yarn", "build"],
+    ["yarn"],
+    ["yarn", "lerna", "bootstrap"],
+    ["yarn", "build"],
 ]:
     rc = subprocess.call(cmd, cwd=str(SRC))
     if rc:
         print("FAIL on", cmd)
         sys.exit(rc)
 
-sys.exist(pytest.main(pytest_args), cwd=str(SRC))
+sys.exist(pytest.main(PYTEST_ARGS), cwd=str(SRC))
